4fbbbe7abd94edaa4618c06cda5bbf5a8eecc5dc
Yuan Chun <cyuanmail@gmail.com>
2016-04-17 21:11:34 +0100

    Add support to minus numbers

--- src/test/java/info/xiaoc/CucumberTest/DepositStepDefinitions.java
+++ src/test/java/info/xiaoc/CucumberTest/DepositStepDefinitions.java
@@ -8,23 +8,24 @@ import cucumber.api.java.en.When;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by ionst on 2016/1/11.
+ * Glue class for deposit/withdraw features.
+ * Created by cyuan on 2016/1/11.
  */
 public class DepositStepDefinitions {
 
     private Account account;
 
-    @Given("^a User has £(\\d+) in their account$")
+    @Given("^a User has £(-?\\d+) in their account$")
     public void aUserHasNoMoneyInTheirAccount(int amount) throws Throwable {
         User user = new User();
         account = new Account();
         account.setBalance(amount);
         user.setAccount(account);
 
-        assertTrue("The balance is not equal to the specified initial balance.", account.getBalance() == Long.valueOf(amount));
+        assertTrue("The balance is not equal to the specified initial balance.", account.getBalance() == amount);
     }
 
-    @When("^£(\\d+) is deposited into the account$")
+    @When("^£(-?\\d+) is deposited into the account$")
     public void £_is_deposited_in_to_the_account(int amount) {
         account.deposit(amount);
     }
@@ -35,7 +36,7 @@ public class DepositStepDefinitions {
                 + account.getBalance(), account.getBalance() == expectedBalance);
     }
 
-    @When("^£(\\d+) is widthdrawn from the account$")
+    @When("^£(-?\\d+) is widthdrawn from the account$")
     public void £_is_widthdrawn_from_the_account(int amount) throws Throwable {
         account.withdraw(amount);
     }
